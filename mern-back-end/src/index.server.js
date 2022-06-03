931a11ac9997b1b02f1205321757c9adeda41b31
Joynamahen <mjoyna4398@gmail.com>
2022-06-03 21:50:05 +0530

    Database

--- mern-back-end/src/index.server.js
+++ mern-back-end/src/index.server.js
@@ -1,10 +1,29 @@
 const express =require('express');
 const env=require('dotenv');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 env.config();
 
-app.use(express.json());
+//mongodb connection 
+//mongodb+srv://root:<password>@cluster0.4ikqksv.mongodb.net/?retryWrites=true&w=majority
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.4ikqksv.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => {
+    console.log("Database connected");
+  });
+
+app.use(bodyParser());
 
 app.get('/', (req,res,next)=>{
   res.status(200).json({
@@ -13,11 +32,11 @@ app.get('/', (req,res,next)=>{
 });
 
 app.post('/data', (req,res,next)=>{
-    res.status(200).json({
-        message: req.body
-    });
+  res.status(200).json({
+      message: req.body
   });
+});
 
-app.listen(process.env.PORT, () =>  {
-    console.log('Server is running on port $ {process.env.PORT} ');
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
