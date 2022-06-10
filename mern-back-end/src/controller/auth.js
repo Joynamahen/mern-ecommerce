bb2554650d5a421c4fc7138f052ebb8804fdfbb2
Joynamahen <mjoyna4398@gmail.com>
2022-06-10 22:34:57 +0530

    create Admin

--- mern-back-end/src/controller/auth.js
+++ mern-back-end/src/controller/auth.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
+//signup
 exports.signup= (req, res) =>{
     User.findOne({email:req.body.email})
     .exec((error,user)=>{
@@ -37,17 +39,40 @@ exports.signup= (req, res) =>{
     });
 }
 
+
+//signin
 exports.signin = (req, res) =>{
    User.findOne({email:req.body.email})
    .exec((error, user) =>{
        if(error) return res.status(400).json({error});
        if(user){
-          if(user.authenticate(req.body.password)){
 
+          if(user.authenticate(req.body.password)){
+               const token=jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h'});
+               const {firstName,lastName,email,role,fullName}  = user;
+               res.status(200).json({
+                  token,
+                  user:{
+                     firstName,lastName,email,role,fullName 
+                  }
+               });
+          }else{
+             return res.status(400).json({
+                message:'Invalid Password'
+             })
           }
 
        }else{
           return res.status(400).json({message:'Something went wrong'});
        }
    });
+}
+
+exports.requireSignin = (req,res,next) =>{
+   const token=req.headers.authorization.split(" ")[1];
+   const user = jwt.verify(token,process.env.JWT_SECRET);
+   req.user = user;
+   console.log(token);
+   next();
+   //jwt.decode{}
 }
