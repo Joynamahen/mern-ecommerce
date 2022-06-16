72a4aca68d1bf4d9eea4c74c416a39ae06d9915c
Joynamahen <mjoyna4398@gmail.com>
2022-06-16 16:08:08 +0530

    form

--- admin-app/src/containers/Signin/index.js
+++ admin-app/src/containers/Signin/index.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 import { Container, Form, Row, Col, Button } from 'react-bootstrap';
+import Input from '../../components/UI/Input';
 
 const Signin = (props) => {
   return (
     <Layout>
       <Container>
-        <Row>
-          <Col md={{span:6 ,offset:3}}>
+        <Row style={{ marginTop: '50px' }}>
+          <Col md={{ span: 6, offset: 3 }}>
             <Form>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
 
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
+              <Input
+                label="Email"
+                placeholder="Email"
+                value=""
+                type="email"
+                onChange={() => { }}
+              />
+
+              <Input
+                label="Password"
+                placeholder="Password"
+                value=""
+                type="password"
+                onChange={() => { }}
+              />
+
+
               <Button variant="primary" type="submit">
                 Submit
               </Button>
