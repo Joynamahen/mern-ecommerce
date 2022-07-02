152bdf4d79d76a93ec869aa4da5665273fffd0b3
Joynamahen <mjoyna4398@gmail.com>
2022-07-02 10:08:33 +0530

    signup,signin done

--- mern-back-end/src/validators/auth.js
+++ mern-back-end/src/validators/auth.js
@@ -1,26 +1,37 @@
-const { check,validationResult } =require ('express-validator');
+const { check, validationResult } = require('express-validator');
 
-exports.validateRequest = [
-check('firstName')
+exports.validateSignupRequest = [    //signup
+  check('firstName')
     .notEmpty()
     .withMessage('firstName is required'),
 
-    check('lastName')
+  check('lastName')
     .notEmpty()
     .withMessage('lastName is required'),
 
-    check('email')
+  check('email')
     .isEmail()
     .withMessage('Valid Email is required'),
 
-    check('password')
-    .isLength({ min : 6})
+  check('password')
+    .isLength({ min: 6 })
     .withMessage('Password must be atleast 6 character long')
 ];
 
-exports.isRequestValidated=(req,res,next)=>{
-      const errors =validationResult(req);
-      if(errors.array().length > 0){
-        return res.status(400).json({ errors: errors.array()[0].msg })    //error print
-      }
+exports.validateSigninRequest = [    //signin
+  check('email')
+    .isEmail()
+    .withMessage('Valid Email is required'),
+
+  check('password')
+    .isLength({ min: 6 })
+    .withMessage('Password must be atleast 6 character long')
+];
+
+exports.isRequestValidated = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.array().length > 0) {
+    return res.status(400).json({ errors: errors.array()[0].msg })    //error print
+  }
+  next();    //user created
 }
